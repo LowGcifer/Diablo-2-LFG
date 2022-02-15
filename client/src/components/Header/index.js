3f91bde2bdb57e01dd8ed41cd7f30a8f228a9a48
LowGcifer <LowGcifer@gmail.com>
2022-02-15 00:55:39 -0500

    Created Header component and Home page, got image to display on home page

---
+++ client/src/components/Header/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Logo from "./LFGLogo.png";
+
+function Header() {
+  return (
+    <div id="desktop-header">
+      <img src={Logo} alt="Diablo 2: Resurrected LFG" />
+    </div>
+  );
+}
+
+export default Header;
