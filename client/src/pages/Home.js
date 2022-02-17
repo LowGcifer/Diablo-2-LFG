14251d4f40c0b4b12665e8d08573eb06c271a4aa
LowGcifer <LowGcifer@gmail.com>
2022-02-17 13:51:04 -0500

    added fonts and started button component

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -6,8 +6,8 @@ const Home = () => {
     <div>
       <Header></Header>
       <h1>
-        Welcome to Diablo 2: <br />
-        Resurrected LFG!
+        Welcome To Diablo 2: <br />
+        ResurrecTed LFG!
       </h1>
       <p id="intro">
         Create your own group with your own parameters, or search for one that
