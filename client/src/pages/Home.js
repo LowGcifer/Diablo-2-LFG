6ac299264ca5ea6da722501b366ba6aacd211dcf
LowGcifer <LowGcifer@gmail.com>
2022-02-17 13:30:24 -0500

    Added a CSS file, added text to home page

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,7 +1,20 @@
 import Header from "../components/Header";
+import "../style.css";
 
 const Home = () => {
-  return <Header />;
+  return (
+    <div>
+      <Header></Header>
+      <h1>
+        Welcome to Diablo 2: <br />
+        Resurrected LFG!
+      </h1>
+      <p id="intro">
+        Create your own group with your own parameters, or search for one that
+        fits your criteria!
+      </p>
+    </div>
+  );
 };
 
 export default Home;
