e637cf5fc74511bcf8c27cfd680a1a8fcb9e013b
LowGcifer <LowGcifer@gmail.com>
2022-02-18 00:42:50 -0500

    Added Parallax effect, started Create Group popup window

---
+++ client/src/components/CreateWindow/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "../../style.css";
+
+function CreateWindow() {
+  return <div id="create-window"></div>;
+}
+
+export default CreateWindow;
--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -1,10 +1,23 @@
 import React from "react";
 import Logo from "./LFGLogo.png";
+// import Parallax from "react-parallax";
+
+// function Header() {
+//   return (
+//     <div>
+//       <Parallax
+//         bgImage={<Logo></Logo>}
+//         bgImageAlt="Diablo 2: Resurrected LFG"
+//         strength={-200}
+//       ></Parallax>
+//     </div>
+//   );
+// }
 
 function Header() {
   return (
     <div id="desktop-header">
-      <img src={Logo} alt="Diablo 2: Resurrected LFG" />
+      {/* <img src={Logo} alt="Diablo 2: Resurrected LFG" /> */}
     </div>
   );
 }
