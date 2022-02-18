0f598cce7c0c6000decb4634e078ee3536981dbf
LowGcifer <LowGcifer@gmail.com>
2022-02-18 13:46:59 -0500

    More heroku

--- client/src/components/CreateWindow/index.js
+++ client/src/components/CreateWindow/index.js
@@ -2,7 +2,13 @@ import React from "react";
 import "../../style.css";
 
 function CreateWindow() {
-  return <div id="create-window"></div>;
+  return (
+    <div id="create-window">
+      <div>
+        <h2>Your Name</h2>
+      </div>
+    </div>
+  );
 }
 
 export default CreateWindow;
